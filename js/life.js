1d8b0d0aa40750cc00fb0e68d0030e24c8233a08
Henry Yeung <henryyeung@theorigo.com>
2019-10-03 18:55:39 +0800

    git init

---
+++ js/life.js
@@ -0,0 +1,45 @@
+var life = new Vue(
+  {
+    el: '#app',
+    vuetify: new Vuetify(),
+    data: {
+      dob: null,
+      error: false,
+      showModal: true
+    },
+    beforeMount(){
+      this.dob = localStorage.getItem('DOB');
+
+      if(this.dob != null){
+        this.showModal = false;
+      }
+    },
+    computed: {
+      max: function(){
+        var fulldate = new Date();
+        return fulldate.toISOString();
+      },
+      today: function(){
+        var fulldate = new Date();
+        return fulldate.toISOString().split('T')[0];
+      },
+      progress: function(){
+        var oneMonth = 30.4 * 24 * 60 * 60 * 1000;
+        var start = new Date(this.dob.split('-')[0], this.dob.split('-')[1], this.dob.split('-')[2],);
+        var end = new Date(this.today.split('-')[0], this.today.split('-')[1], this.today.split('-')[2],);
+        return Math.round(Math.abs((end - start) / oneMonth));
+      }
+    },
+    methods: {
+      save: function(){
+        if (this.dob == null){
+          this.showModal = true;
+          this.error = true;
+        } else {
+          localStorage.setItem('DOB', this.dob);
+          this.showModal = false;
+        }
+      }
+    }
+  }
+)
